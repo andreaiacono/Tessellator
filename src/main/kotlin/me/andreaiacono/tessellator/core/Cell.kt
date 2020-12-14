8640db17b3987967f22331f77e710e5005a54eda
Andrea Iacono <andrea.iacono.nl@gmail.com>
2020-12-14 11:48:06 +0100

    refactoring

--- src/main/kotlin/me/andreaiacono/tessellator/core/Cell.kt
+++ src/main/kotlin/me/andreaiacono/tessellator/core/Cell.kt
@@ -1,6 +1,5 @@
 package me.andreaiacono.tessellator.core
 
-import java.text.DecimalFormat
 import kotlin.math.abs
 import kotlin.math.sqrt
 
@@ -56,22 +55,3 @@ class Cell {
             }
     }
 }
-
-data class ScaledPoint(val x: Int, val y: Int)
-data class Point(var x: Double, var y: Double, var isMoving: Boolean = false) {
-    val dec = DecimalFormat("#.####")
-    override fun toString(): String = "(${dec.format(x)}, ${dec.format(y)})"
-
-    constructor(coords: Coords, size: Int) : this(
-        (coords.x % size) / size.toDouble(),
-        1.0 - (coords.y % size) / size.toDouble()
-    )
-
-    fun updatePosition(current: Coords, starting: Coords, size: Int) {
-        x = (starting.x %size) / size.toDouble() + (((current.x - starting.x))) / size.toDouble()
-        y = 1.0 - (starting.y %size) / size.toDouble() - (((current.y - starting.y))) / size.toDouble()
-    }
-}
-
-fun Point.scale(width: Int, height: Int): ScaledPoint = ScaledPoint((this.x * width).toInt(), (this.y * height).toInt())
-
---
+++ src/main/kotlin/me/andreaiacono/tessellator/core/Point.kt
@@ -0,0 +1,24 @@
+package me.andreaiacono.tessellator.core
+
+import java.text.DecimalFormat
+
+data class Point(var x: Double, var y: Double, var isMoving: Boolean = false) {
+    val dec = DecimalFormat("#.####")
+    override fun toString(): String = "(${dec.format(x)}, ${dec.format(y)})"
+
+    constructor(coords: Coords, size: Int) : this(
+        (coords.x % size) / size.toDouble(),
+        1.0 - (coords.y % size) / size.toDouble()
+    )
+
+    fun updatePosition(current: Coords, starting: Coords, size: Int) {
+        x = (starting.x %size) / size.toDouble() + (((current.x - starting.x))) / size.toDouble()
+        y = 1.0 - (starting.y %size) / size.toDouble() - (((current.y - starting.y))) / size.toDouble()
+    }
+
+    fun scale(width: Int, height: Int): ScaledPoint = ScaledPoint((this.x * width).toInt(), (this.y * height).toInt())
+}
+
+data class ScaledPoint(val x: Int, val y: Int)
+
+
